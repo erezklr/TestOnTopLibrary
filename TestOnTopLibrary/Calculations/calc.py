f5b84b1abf5ea186cc6636700cedd40f4288acc9
erezklr <erezklr@users.noreply.github.com>
2024-04-17 20:53:20 +0200

    Add files via upload

---
+++ TestOnTopLibrary/Calculations/calc.py
@@ -0,0 +1,12 @@
+def sum(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2):
+	return num1 - num2
+
+def mul(num1, num2):
+	return num1 * num2
+
+def div(num1, num2):
+	return num1 / num2
+    
---
+++ TestOnTopLibrary/main.py
@@ -0,0 +1,8 @@
+# Calculations.calc import sum
+
+from Calculations.calc import *
+
+print(sum(1,2))
+print(sub(100,2))
+print(mul(12,2))
+print(div(10,5))
